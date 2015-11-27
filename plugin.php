d1b384e53666c506c78b4e1cc417257767c7f016
Armin Braun <armin.b@icanlocalize.com>
2015-11-27 13:29:47 +0100

    Basic plugin setup

---
+++ plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WPML Term Meta Mock
+Plugin URI: https://wpml.org/
+Description: Allows testing term meta translation via WPML
+Author: OnTheGoSystems
+Author URI: http://www.onthegosystems.com/
+Version: 0.1
+Plugin Slug: wpml-term-meta-mock
+*/
+
+define( 'WPML_TERM_META_MOCK_VERSION', '0.1' );
